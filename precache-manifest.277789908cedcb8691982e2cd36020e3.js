bf8cdb95d24dcb33f51a835b4e566bd467703359
Mohamed Assem <m.assem1997@gmail.com>
2021-01-08 00:52:23 +0200

    Add files via upload

---
+++ precache-manifest.277789908cedcb8691982e2cd36020e3.js
@@ -0,0 +1,542 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9914c8ce617cbfbf3a3437cbd3dd72d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a3d70d62fa1abd7044",
+    "url": "/static/css/2.e7efe8d3.chunk.css"
+  },
+  {
+    "revision": "6c6147185eb3389266e8",
+    "url": "/static/css/main.d12d9970.chunk.css"
+  },
+  {
+    "revision": "11a3d70d62fa1abd7044",
+    "url": "/static/js/2.dbfb0ce2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dbfb0ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6147185eb3389266e8",
+    "url": "/static/js/main.b8ad081b.chunk.js"
+  },
+  {
+    "revision": "29667d8be4419c6e3357",
+    "url": "/static/js/runtime-main.7b1fef25.js"
+  },
+  {
+    "revision": "04176fc7940ed03d50671e984447611c",
+    "url": "/static/media/01.04176fc7.jpg"
+  },
+  {
+    "revision": "89c2b453e4508b3151cb1e0b84781a74",
+    "url": "/static/media/01.89c2b453.jpg"
+  },
+  {
+    "revision": "8d408a8e735e92c5e721faed410049fb",
+    "url": "/static/media/01.8d408a8e.png"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/01.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "/static/media/01.ab7662de.jpg"
+  },
+  {
+    "revision": "fdf18d72f8a8f0db45d42bfe9bc72d17",
+    "url": "/static/media/01.fdf18d72.png"
+  },
+  {
+    "revision": "3787ca22ae9cb7ffea3e2cfcd8aa8159",
+    "url": "/static/media/02.3787ca22.png"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/static/media/02.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/02.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "/static/media/02.ab7662de.jpg"
+  },
+  {
+    "revision": "cb0e49fc4d87138f27499a23e6e2fce2",
+    "url": "/static/media/02.cb0e49fc.jpeg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/static/media/03.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/03.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "/static/media/03.ab7662de.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/static/media/04.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/04.99492839.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/static/media/05.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/05.99492839.jpg"
+  },
+  {
+    "revision": "50c5a5a7a9e816933ee11099ea4d3a48",
+    "url": "/static/media/06.50c5a5a7.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/06.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/07.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/08.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/09.99492839.jpg"
+  },
+  {
+    "revision": "269c71f648fdc17b728c0e7cfc6f98fc",
+    "url": "/static/media/1.269c71f6.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/10.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/11.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/12.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/13.99492839.jpg"
+  },
+  {
+    "revision": "9dc3deb14c145415f91d78298a31bd7b",
+    "url": "/static/media/1306.9dc3deb1.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/14.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "/static/media/15.99492839.jpg"
+  },
+  {
+    "revision": "269c71f648fdc17b728c0e7cfc6f98fc",
+    "url": "/static/media/2.269c71f6.jpg"
+  },
+  {
+    "revision": "ad00bf958c4e508803b077a8ac3676bd",
+    "url": "/static/media/2150.ad00bf95.jpg"
+  },
+  {
+    "revision": "829d452c64c4b55779e2140db2c59edb",
+    "url": "/static/media/404.829d452c.png"
+  },
+  {
+    "revision": "4cd03c66f98a788068c1e205ec376c31",
+    "url": "/static/media/4898112_4025103_0281010254_ecm_ecu_for_cummins_isbe_isde_diesel_engine_parts_ecu_ecm.4cd03c66.jpg"
+  },
+  {
+    "revision": "fb558efb53447d74a861e5b7aa238bcd",
+    "url": "/static/media/60100.fb558efb.jpg"
+  },
+  {
+    "revision": "5c3cd10d25b2da42990dca491cb32cd8",
+    "url": "/static/media/CUMM.5c3cd10d.jpg"
+  },
+  {
+    "revision": "6fd43fa46607599d63378f7ec90bddd3",
+    "url": "/static/media/Caterpillar.6fd43fa4.png"
+  },
+  {
+    "revision": "f9969487403be80a3992e0c6ed845a70",
+    "url": "/static/media/Caterpillar.f9969487.png"
+  },
+  {
+    "revision": "6fd43fa46607599d63378f7ec90bddd3",
+    "url": "/static/media/Caterpillar_logo.svg.6fd43fa4.png"
+  },
+  {
+    "revision": "0b6dfa23dad779329a35de7c96130843",
+    "url": "/static/media/DCU.0b6dfa23.jpg"
+  },
+  {
+    "revision": "441ab2ba4dd438e57aa60572d34907d7",
+    "url": "/static/media/ECM.441ab2ba.jpg"
+  },
+  {
+    "revision": "6950e26b00da02e27a30165820abe3d5",
+    "url": "/static/media/F_G_Wilson_logo.com.6950e26b.png"
+  },
+  {
+    "revision": "99cf4a2cb0023f676a60cf2efd7c003d",
+    "url": "/static/media/PEEC.99cf4a2c.jpg"
+  },
+  {
+    "revision": "e10ee9c7fb8d872bf532387aa2e2dbf3",
+    "url": "/static/media/PERK.e10ee9c7.jpg"
+  },
+  {
+    "revision": "a903bc402d9f38a6c56b9ed70bd4c7d2",
+    "url": "/static/media/TCL.a903bc40.png"
+  },
+  {
+    "revision": "c7f7b654836bcd6158f6c02ba3c0e8b6",
+    "url": "/static/media/Volvo-Logo.c7f7b654.jpeg"
+  },
+  {
+    "revision": "f9a6de9672f9f3ff9e682f25bb8d6f81",
+    "url": "/static/media/YAS-Egypt-51086-1578398912-og.f9a6de96.png"
+  },
+  {
+    "revision": "efa7bcb92aa2249bf537ec55c9890b0d",
+    "url": "/static/media/YAS2_noBG.efa7bcb9.png"
+  },
+  {
+    "revision": "ce235ce5f16c41f68857cb2ccade1598",
+    "url": "/static/media/YAS_noBG.ce235ce5.png"
+  },
+  {
+    "revision": "7e76bab945c624e803a6382fb4905fd8",
+    "url": "/static/media/about-2-bg-.7e76bab9.jpg"
+  },
+  {
+    "revision": "1aaab2ca1ac44db4b509402e8908cdeb",
+    "url": "/static/media/about-2-bg.1aaab2ca.jpg"
+  },
+  {
+    "revision": "011111bdd037740918516ee25bafecfc",
+    "url": "/static/media/about-2-bg_old.011111bd.jpg"
+  },
+  {
+    "revision": "25f6c6270afca0b034de52f3a4dc495f",
+    "url": "/static/media/about-2.25f6c627.jpg"
+  },
+  {
+    "revision": "e5ae11aaf60884c8efaf836097345fd2",
+    "url": "/static/media/about-2_old.e5ae11aa.jpg"
+  },
+  {
+    "revision": "25f6c6270afca0b034de52f3a4dc495f",
+    "url": "/static/media/about.25f6c627.jpg"
+  },
+  {
+    "revision": "04d2d2f82017ebad62e038d7266a548b",
+    "url": "/static/media/about_home_indust.04d2d2f8.jpg"
+  },
+  {
+    "revision": "97310a09d84741a74f4ee18efbfdd4b9",
+    "url": "/static/media/adec.97310a09.jpg"
+  },
+  {
+    "revision": "e011fe14c6e054ebf6ca5a2956b8044e",
+    "url": "/static/media/adem3.e011fe14.jpg"
+  },
+  {
+    "revision": "89760db267260aef62feb7d094296ba5",
+    "url": "/static/media/adem4.89760db2.jpg"
+  },
+  {
+    "revision": "a0554567df1c63f9d6d1bfc72022d0fb",
+    "url": "/static/media/adem_2.a0554567.jpg"
+  },
+  {
+    "revision": "70106fbe190969bb5911cb6d294ba8eb",
+    "url": "/static/media/adem_3.70106fbe.jpg"
+  },
+  {
+    "revision": "f9854ca096248bc26920fbb71b4262bb",
+    "url": "/static/media/adem_4.f9854ca0.jpg"
+  },
+  {
+    "revision": "66d9dd5e95325fa52fc695dbfec8c9bc",
+    "url": "/static/media/al_marasem.66d9dd5e.jpeg"
+  },
+  {
+    "revision": "17a1b230bf29246af179e1df091f41ab",
+    "url": "/static/media/blog-r-g.17a1b230.jpg"
+  },
+  {
+    "revision": "79eec1ff84fe87852749db7442c246cb",
+    "url": "/static/media/bomag.79eec1ff.jpg"
+  },
+  {
+    "revision": "02cee1a5e3ed455c72e415858e12eeea",
+    "url": "/static/media/c6-6.02cee1a5.jpg"
+  },
+  {
+    "revision": "d9b7fd413f2130aa00f789adc5118d6c",
+    "url": "/static/media/cat1.d9b7fd41.jpg"
+  },
+  {
+    "revision": "43bf710352106dbf6a748d03eb07d4ed",
+    "url": "/static/media/cat2.43bf7103.jpg"
+  },
+  {
+    "revision": "c48c90e54190214a3292aa7ec412d8ad",
+    "url": "/static/media/cat3.c48c90e5.jpg"
+  },
+  {
+    "revision": "b6a766276d1c4ab0fa7b7b129e447e5a",
+    "url": "/static/media/cumm.b6a76627.jpg"
+  },
+  {
+    "revision": "e26280985ea7b1a67ff785e5ff395aa3",
+    "url": "/static/media/cumm59.e2628098.jpg"
+  },
+  {
+    "revision": "bc0b885e61ad39ffc133808ebf371381",
+    "url": "/static/media/cummism.bc0b885e.jpg"
+  },
+  {
+    "revision": "207c7c31d9d629eb4109dd3d86237341",
+    "url": "/static/media/ddec3.207c7c31.jpg"
+  },
+  {
+    "revision": "b6ba5baf0bba2f7f5b80dc6ff949f4a1",
+    "url": "/static/media/ddec4.b6ba5baf.jpg"
+  },
+  {
+    "revision": "1c20d5be22eeecd833b933456d2a77e5",
+    "url": "/static/media/display.1c20d5be.jpg"
+  },
+  {
+    "revision": "b0f2ed76efe84e0e2c9fab0de5bedcb1",
+    "url": "/static/media/dse4510.b0f2ed76.jpg"
+  },
+  {
+    "revision": "ee59e3bb34e689c9e32f80a9f3f19494",
+    "url": "/static/media/dse4610.ee59e3bb.jpg"
+  },
+  {
+    "revision": "787fec9d21d0cfbd11bd6a46257094c4",
+    "url": "/static/media/dse6110.787fec9d.jpg"
+  },
+  {
+    "revision": "97c7d3046d8f25dcec21a1ca9f43f893",
+    "url": "/static/media/dse7110.97c7d304.jpg"
+  },
+  {
+    "revision": "f07a012f59f420a05cc3d4caead8d3d3",
+    "url": "/static/media/dse7310.f07a012f.jpg"
+  },
+  {
+    "revision": "06f500539c90b60d0e0c17ce36b484a4",
+    "url": "/static/media/dse7410.06f50053.jpg"
+  },
+  {
+    "revision": "e99df46613559287120a2c602358bad5",
+    "url": "/static/media/dynapac.e99df466.jpg"
+  },
+  {
+    "revision": "62907149bf78b6d195ff22f8a597fe9e",
+    "url": "/static/media/emcp2.62907149.jpg"
+  },
+  {
+    "revision": "75c6bc1b889c28a47655736a1b1cbdaf",
+    "url": "/static/media/emcp33.75c6bc1b.jpg"
+  },
+  {
+    "revision": "882c5a0b5e3d3a5faa91fb1bcaabf140",
+    "url": "/static/media/emcp41.882c5a0b.jpg"
+  },
+  {
+    "revision": "17c07ef7c89171a4639c8b24125ad8df",
+    "url": "/static/media/ems2.17c07ef7.jpg"
+  },
+  {
+    "revision": "fe6f97cd7ce74d45e8f08937dd19cc3d",
+    "url": "/static/media/exv.fe6f97cd.jpg"
+  },
+  {
+    "revision": "d006437becb84e03525bf006190b2a32",
+    "url": "/static/media/favicon.d006437b.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "34c6010a2a87fcd287f2680335bc516a",
+    "url": "/static/media/fun-fact-bg.34c6010a.jpg"
+  },
+  {
+    "revision": "269c71f648fdc17b728c0e7cfc6f98fc",
+    "url": "/static/media/h-2-01.269c71f6.jpg"
+  },
+  {
+    "revision": "bbc1807e3f534526e9f1d92823aca46d",
+    "url": "/static/media/h-2-02.bbc1807e.jpg"
+  },
+  {
+    "revision": "08dedbcbecba6fc1c511905dcd6bdde0",
+    "url": "/static/media/ind.08dedbcb.jpg"
+  },
+  {
+    "revision": "49be26c9dfca4967914ed8c44f411a48",
+    "url": "/static/media/joysticks.49be26c9.jpg"
+  },
+  {
+    "revision": "9ec09dc12c82aa03036c135447acaf22",
+    "url": "/static/media/logo.9ec09dc1.png"
+  },
+  {
+    "revision": "08c5da44ff81492f2ef688589135af1d",
+    "url": "/static/media/logo_old.08c5da44.png"
+  },
+  {
+    "revision": "f7c5e50c181c74caca8183e0bd9c0617",
+    "url": "/static/media/logobig.f7c5e50c.png"
+  },
+  {
+    "revision": "002eadab883d95bff0b0dc3009f9748d",
+    "url": "/static/media/mdec.002eadab.jpg"
+  },
+  {
+    "revision": "38dd1bcca092fea43fcc3f3c4de50a98",
+    "url": "/static/media/med.38dd1bcc.jpg"
+  },
+  {
+    "revision": "2eead82b0626fdc1688efd3d2b7247bb",
+    "url": "/static/media/page-header.2eead82b.jpg"
+  },
+  {
+    "revision": "118275c1093bba01cee6c2c54b3e7193",
+    "url": "/static/media/page-header_old.118275c1.jpg"
+  },
+  {
+    "revision": "6a1d25e6c41238c85e806a79c69c10be",
+    "url": "/static/media/photo5829926575833986160.6a1d25e6.jpg"
+  },
+  {
+    "revision": "0b1cace3428ae87df1421a3d51ee3271",
+    "url": "/static/media/photo5861754396621255678.0b1cace3.jpg"
+  },
+  {
+    "revision": "2e54e61b52e7b1f822dfae410c4b4df0",
+    "url": "/static/media/photo5861754396621255679.2e54e61b.jpg"
+  },
+  {
+    "revision": "9587c6b1a10fe1beb240223062847e27",
+    "url": "/static/media/photo5861754396621255680.9587c6b1.jpg"
+  },
+  {
+    "revision": "72e9fe90c5fe8cd645dd81ac1dda567d",
+    "url": "/static/media/photo5861754396621255681.72e9fe90.jpg"
+  },
+  {
+    "revision": "6a44d26e2899fade35a97cee726f90aa",
+    "url": "/static/media/pwzemcp10.6a44d26e.jpg"
+  },
+  {
+    "revision": "ebaa227cb7d33edba218a60c725d044d",
+    "url": "/static/media/pwzemcp11.ebaa227c.jpg"
+  },
+  {
+    "revision": "9d2e43fd3d1bc34e50362880b924907a",
+    "url": "/static/media/pwzemcp21.9d2e43fd.jpg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "56bb5bdf6cde9372c4a19e0112ba996e",
+    "url": "/static/media/rsz_volvo-logo.56bb5bdf.jpg"
+  },
+  {
+    "revision": "1a0ae75dcd69a632f8c9cef7e03babf1",
+    "url": "/static/media/service-bg.1a0ae75d.jpg"
+  },
+  {
+    "revision": "aca54e4d62bf00e7ef808e315e33ff03",
+    "url": "/static/media/slider-bg.aca54e4d.jpg"
+  },
+  {
+    "revision": "3043e148f57e860c3c035404087034cb",
+    "url": "/static/media/soon.3043e148.jpg"
+  },
+  {
+    "revision": "9e1de3c7991ceec158b70e136c8aa429",
+    "url": "/static/media/soon.9e1de3c7.png"
+  },
+  {
+    "revision": "a8d6769ca9c2b5df2715525ae4a7a6c4",
+    "url": "/static/media/ss.a8d6769c.bin"
+  },
+  {
+    "revision": "5ed35a47e7bc6ab6952239ef11841642",
+    "url": "/static/media/team-bg-2.5ed35a47.jpg"
+  },
+  {
+    "revision": "efdc4e1a3d8b94a3c195efe58f743069",
+    "url": "/static/media/team-bg.efdc4e1a.jpg"
+  },
+  {
+    "revision": "3f284518090f05a859af1e6418a56978",
+    "url": "/static/media/team-details.3f284518.jpg"
+  },
+  {
+    "revision": "309edc077941230611f1dd502ef2d38b",
+    "url": "/static/media/team.309edc07.jpg"
+  },
+  {
+    "revision": "9d70942328708702ed40c89f8f94f694",
+    "url": "/static/media/volv1.9d709423.jpg"
+  },
+  {
+    "revision": "88c36f34c08e171f604e50411dbbc5e4",
+    "url": "/static/media/volvo_excavator.88c36f34.jpg"
+  },
+  {
+    "revision": "ce235ce5f16c41f68857cb2ccade1598",
+    "url": "/static/media/yasbg.ce235ce5.png"
+  }
+]);
